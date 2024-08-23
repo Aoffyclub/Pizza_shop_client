23184f65c51eb51b3aeef5c0291283f7453cb4a7
Aoffy <99707911+Aoffyclub@users.noreply.github.com>
2024-08-23 14:04:13 +0700

    update  category

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -4,38 +4,81 @@ import toast from "react-hot-toast";
 
 import TypingAnimation from "@/components/magicui/typing-animation";
 import MenuCard from "@/components/MenuCard";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const Menu = () => {
   const [dataProduct, setDataProduct] = useState([]);
-
+  const [prductCategory, setProductCategory] = useState([]);
+  const [category, setCategory] = useState(null);
 
   const getMenu = async () => {
     try {
-      const res = await axios.get(
-        import.meta.env.VITE_BASE_API + "/api/product"
-      );
-      setDataProduct(res.data.data);
+      const res = await axios
+        .get(import.meta.env.VITE_BASE_API + "/api/product")
+        .then((res) => {
+          const uniqueCategories = res.data.data.reduce((acc, item) => {
+            if (!acc.includes(item.category_id)) {
+              acc.push(item.category_id);
+            }
+            return acc;
+          }, []);
+
+          setCategory(uniqueCategories);
+          setDataProduct(res.data.data);
+          setProductCategory(res.data.data);
+        });
     } catch (err) {
       toast.error("Failed to fetch menu.");
       console.error(err);
     }
   };
 
-   useEffect(() => {
-     getMenu();
-   }, []);
+  useEffect(() => {
+    getMenu();
+  }, []);
+
+  const selectCategory = (value) => {
+    setProductCategory(
+      dataProduct.filter((product) => product.category_id === value)
+    );
+  };
 
   return (
     <div className="flex flex-col gap-3 sm:px-10 px-4 sm:mt-10 mt-4 items-center sm:items-start">
-      <div className="flex pl-10">
+      <div className="flexpl-10">
         <TypingAnimation
           text={"Pizza menu 🛒"}
-          className="text-xl md:text-2xl lg:text-2xl xl:text-3xl font-bold"
+          className="text-xl md:text-2xl lg:text-2xl xl:text-3xl font-bold "
         />
+      </div>
+      <div className="md:text-lg text-base font-bold ">
+        <Select onValueChange={selectCategory}>
+          <SelectTrigger>
+            <SelectValue placeholder="Category" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Category</SelectLabel>
+              {category?.map((data, index) => (
+                <SelectItem value={data}>
+                  {data.charAt(0).toUpperCase() + data.slice(1)}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </div>
 
       <div className="grid md:grid-cols-2 grid-cols-1 gap-6 px-4 mt-5">
-        {dataProduct?.map((data) => (
+        {prductCategory?.map((data) => (
           <MenuCard data={data} />
         ))}
       </div>
